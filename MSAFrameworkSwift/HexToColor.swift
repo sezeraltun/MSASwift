f1fdd34f2cf7c01a981585f40610929062a9cf6c
sezeraltun <sezer.altun@gmail.com>
2020-04-18 01:11:08 +0300

    Create Framework

---
+++ MSAFrameworkSwift/HexToColor.swift
@@ -0,0 +1,15 @@
+//
+//  HexToColor.swift
+//  MSAFrameworkSwift
+//
+//  Created by MacBook Pro on 18.04.2020.
+//  Copyright © 2020 Sezer Altun. All rights reserved.
+//
+
+import Foundation
+
+
+class HexToColor{
+    
+    
+}
---
+++ MSAFrameworkSwift/MSAFrameworkSwift.h
@@ -0,0 +1,19 @@
+//
+//  MSAFrameworkSwift.h
+//  MSAFrameworkSwift
+//
+//  Created by MacBook Pro on 18.04.2020.
+//  Copyright © 2020 Sezer Altun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSAFrameworkSwift.
+FOUNDATION_EXPORT double MSAFrameworkSwiftVersionNumber;
+
+//! Project version string for MSAFrameworkSwift.
+FOUNDATION_EXPORT const unsigned char MSAFrameworkSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSAFrameworkSwift/PublicHeader.h>
+
+
